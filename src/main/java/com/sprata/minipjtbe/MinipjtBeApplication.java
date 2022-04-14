ac1021f5d14153bed6c17d65dcf770720636092c
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-04-14 18:44:16 +0900

    고생하셨습니다

--- src/main/java/com/sprata/minipjtbe/MinipjtBeApplication.java
+++ src/main/java/com/sprata/minipjtbe/MinipjtBeApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class MinipjtBeApplication {
+
+    @PostConstruct
+    public void time() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
         + "classpath:/aws.yml"
